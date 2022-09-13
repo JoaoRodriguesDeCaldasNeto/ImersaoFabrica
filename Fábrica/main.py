ff97c596d894e1a517396e0255c272306d473274
João Rodrigues de Caldas Neto <113389927+JoaoRodriguesDeCaldasNeto@users.noreply.github.com>
2022-09-13 12:05:47 -0300

    Add files via upload
    Aula 01

---
+++ Fábrica/main.py
@@ -0,0 +1,10 @@
+from sec import funcao
+
+base = int (0)
+altura =int (0)
+AreaT = (0)
+
+base=int(input("Digite o valor de sua base: "))
+altura=int(input("Digite o valor de sua altura: "))
+
+funcao(base, altura)
---
+++ Fábrica/sec.py
@@ -0,0 +1,5 @@
+def funcao (base, altura):
+
+    AreaT = (base*altura)*(0.5)
+    
+    print ("O valor da area é de"+"",AreaT)
